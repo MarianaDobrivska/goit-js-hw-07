1675de86bcfae2441d14a8246c29b85bed97f8f0
MarianaDobrivska <dobrivska.marry@gmail.com>
2022-11-02 17:35:09 +0200

    update 1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const newDiv = document.createElement("div");
+  const newMarkup = `<a class="gallery__link" href=${item.original}>
+              <img
+                class="gallery__image"
+                src=${item.preview}
+                data-source=${item.original}
+                alt=${item.description}
+              />
+            </a>`;
+
+  newDiv.innerHTML = newMarkup;
+  //   console.log(newDiv);
+  galleryEl.append(newDiv);
+});
+
+galleryEl.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return "";
+
+  const clickedImage = e.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${clickedImage}" width="800" height="600">
+`);
+
+  instance.show();
+}
+
+// function onBackdropClick(e) {
+//   if (e.code === "Escape") {
+//     galleryEl.removeEventListener("click", onBackdropClick);
+//   }
+// }
